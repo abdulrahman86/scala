0b9220752eea4b19af05f269350475d911411663
Abdul Sattar <asattar@exchangesolutions.com>
2016-04-16 14:56:05 -0400

    Option and Either datastructure

--- learningscala/src/main/scala/fpInScala/c4/Option.scala
+++ learningscala/src/main/scala/fpInScala/c4/Option.scala
@@ -2,15 +2,21 @@ package fpInScala.c4
 
 import java.util.regex.{Pattern, PatternSyntaxException}
 
-trait Option[+A] {
-  def map[B](f: A => B): Option[B] = None
-  def flatMap[B](f: A => Option[B]): Option[B] = None
-  def getOrElse[B >: A](default: => B): B = default
-  def orElse[B >: A](ob: => Option[B]): Option[B] = None
-  def filter(f: A => Boolean): Option[A] = None
+sealed trait Option[+A] {
+  def map[B](f: A => B): Option[B]
+  def flatMap[B](f: A => Option[B]): Option[B]
+  def getOrElse[B >: A](default: => B): B
+  def orElse[B >: A](ob: => Option[B]): Option[B]
+  def filter(f: A => Boolean): Option[A]
 }
 
-final case object None extends Option[Nothing];
+final case object None extends Option[Nothing] {
+  override def map[B](f: Nothing => B): Option[B] = None
+  override def flatMap[B](f: Nothing => Option[B]): Option[B] = None
+  override def getOrElse[B >: Nothing](default: => B): B = default
+  override def orElse[B >: Nothing](ob: => Option[B]): Option[B] = None
+  override def filter(f: Nothing => Boolean): Option[Nothing] = None
+}
 
 final case class Some[A](value: A) extends Option[A] {
   override def map[B](f: A => B): Option[B] =  Some(f(value))
@@ -18,6 +24,26 @@ final case class Some[A](value: A) extends Option[A] {
   override def getOrElse[B >: A](default: => B): B = value
   override def orElse[B >: A](ob: => Option[B]): Option[B] = this
   override def filter(f: A => Boolean): Option[A] = flatMap(x => if (f(x)) Some(x) else None)
+}
+
+sealed trait Either[+E, +A] {
+  def map[B](f: A => B): Either[E, B]
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B]
+  def orElse[EE >: E,B >: A](b: => Either[EE, B]): Either[EE, B]
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C):
+  Either[EE, C]
+}
+case class Left[+E](value: E) extends Either[E, Nothing] {
+  override def map[B](f: Nothing => B): Either[E, B] = this
+  override def flatMap[EE >: E, B](f: Nothing => Either[EE, B]): Either[EE, B] = this
+  override def orElse[EE >: E,B >: Nothing](b: => Either[EE, B]): Either[EE, B] = b
+  override def map2[EE >: E, B, C](b: Either[EE, B])(f: (Nothing, B) => C) : Either[EE, C] = this
+}
+case class Right[+A](value: A) extends Either[Nothing, A] {
+  override def map[B](f: A => B): Either[Nothing, B] = Right(f(value))
+  override def flatMap[EE >: Nothing, B](f: A => Either[EE, B]): Either[EE, B] = f(value)
+  override def orElse[EE >: Nothing,B >: A](b: => Either[EE, B]): Either[EE, B] = this
+  override def map2[EE >: Nothing, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = b map(f(value, _))
 }
 
 object Option {
@@ -53,6 +79,19 @@ object Option {
   def sequence2[A](l: List[Option[A]]): Option[List[A]] =
     traverse[Option[A], A](l)((x: Option[A]) => x)
 
-
   def of[A](value: A) : Option[A] = Some(value)
+}
+
+object Either {
+
+  def Try[A](x : => A) : Either[Exception, A] =
+    try Right(x)
+    catch {case e : Exception => Left(e)}
+
+  def traverse[E, A, B](as: List[A])(f: A => Either[E, B]): Either[E, List[B]] =
+    as.foldLeft[Either[E,List[B]]](Right(Nil))((x, y) => x flatMap (arg1 => f(y) map (arg2 =>  (arg2 :: (arg1 reverse)) reverse)))
+
+  def sequence[E, A] (es: List[Either[E, A]]) : Either[E, List[A]] =
+    traverse(es)(x => x)
+
 }
