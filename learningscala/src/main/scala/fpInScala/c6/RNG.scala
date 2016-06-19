561adb59d10f04bdce7904e6b5566b92f5c674d3
Abdul Sattar <asattar@exchangesolutions.com>
2016-06-19 14:05:28 -0400

    more rng.

--- learningscala/src/main/scala/fpInScala/c6/RNG.scala
+++ learningscala/src/main/scala/fpInScala/c6/RNG.scala
@@ -17,6 +17,36 @@ case class SimpleRNG(seed: Long) extends RNG {
 
 object RNG {
 
+  type Rand[+A] = RNG => (A, RNG)
+
+  val int: Rand[Int] = _.nextInt
+
+  def unit[A](a: A) : Rand[A] =
+    rng => (a, rng)
+
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] =
+    rng => {
+      val (a, rRng) = s(rng)
+      (f(a), rng)
+    }
+
+  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    rng => {
+      val (a, rRng1) = ra(rng)
+      val (b, rRng2) = rb(rRng1)
+      (f(a, b), rRng2)
+    }
+
+  def both[A, B](ra: Rand[A], rb: Rand[B]): Rand[(A, B)] =
+    map2(ra, rb)((_, _))
+
+
+  def nonNegativeEven: Rand[Int] =
+    map(nonNegativeInt)(i => i - i % 2)
+
+  def double2: Rand[Double] =
+    map(int)(_.toDouble / Integer.MAX_VALUE)
+
   @tailrec
   def nonNegativeInt(rng: RNG): (Int, RNG) = rng.nextInt match {
     case (x, y) if x != 0 => (Math.abs(x),y)
@@ -43,4 +73,38 @@ object RNG {
       val(tail, rng2) = ints(count - 1)(rng1)
       (r1 :: tail, rng2)
     }
+
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
+    fs.foldRight[Rand[List[A]]](unit(List())){(x: Rand[A], y: Rand[List[A]]) =>{
+      (rng: RNG) => {
+        val (tail, rng1) = y(rng)
+        val (head, rng2) = x(rng1)
+        (head :: tail, rng2)
+      }
+    }}
+
+  def ints2(count: Int)(rng: RNG): (List[Int], RNG) =
+    sequence(List.fill(count)(int))(rng)
+
+  def flatMap[A, B](f: Rand[A])(g: A => Rand[B]): Rand[B] =
+    rng => {
+      val (a, rngNext) = f(rng)
+      g(a)(rngNext)
+    }
+
+  def map3[A, B](s: Rand[A])(f: A => B): Rand[B] =
+    flatMap[A, B](s)(x => unit(f(x)))
+
+  def map4[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    map3[(A, B), C](flatMap[A, (A,B)](ra)(a => {
+      rng => {
+        val (b, r) = rb(rng)
+        ((a, b),r)
+      }
+    }))(x => f(x._1, x._2))
+
+  def both2[A, B](ra: Rand[A], rb: Rand[B]): Rand[(A, B)] =
+    map4(ra, rb)((_, _))
+
+
 }
--- learningscala/src/main/scala/fpInScala/c6/c6.sc
+++ learningscala/src/main/scala/fpInScala/c6/c6.sc
@@ -8,3 +8,7 @@ RNG.double3(SimpleRNG(System.currentTimeMillis()))
 
 RNG.ints(5)(SimpleRNG(System.currentTimeMillis()))
 
+RNG.ints2(5)(SimpleRNG(System.currentTimeMillis()))
+
+RNG.both2(RNG.int, RNG.double2)(SimpleRNG(System.currentTimeMillis()))
+
