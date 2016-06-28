dc9d963fa6d5bea303ad738de885e6ad9ff6e09f
Abdul Sattar <asattar@exchangesolutions.com>
2016-06-28 07:39:58 -0400

    candy state machine generic state

---
+++ learningscala/src/main/scala/fpInScala/c6/CandyMachine.scala
@@ -0,0 +1,8 @@
+package fpInScala.c6
+
+/**
+  * Created by asattar on 2016-06-28.
+  */
+class CandyMachine {
+
+}
---
+++ learningscala/src/main/scala/fpInScala/c6/State.scala
@@ -0,0 +1,8 @@
+package fpInScala.c6
+
+/**
+  * Created by asattar on 2016-06-26.
+  */
+class State {
+
+}
